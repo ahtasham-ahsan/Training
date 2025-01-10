c1bf3a7b3dff500a0a61129b63f311f83cedbd71
haxhmi1993 <127983564+haxhmi1993@users.noreply.github.com>
2025-01-10 16:07:15 +0500

    Factory Fucntions

---
+++ 07 FactoryFunctions.js
@@ -0,0 +1,36 @@
+
+const elf = {
+    name: 'Orwell', 
+    weapon: 'Bow', 
+    attack(){
+        return 'Attack with ' + elf.weapon
+    }
+}
+console.log(elf.attack());
+
+const elf2 = {
+    name: 'Sally', 
+    weapon: 'Knife', 
+    attack(){
+        return 'Attack with ' + elf2.weapon
+    }
+}
+console.log(elf2.attack());
+
+// Factory Functions 
+function createElf(name, weapon){
+    return {
+        name: name,
+        weapon: weapon,
+        attack(){
+            return name + ' Attack with ' + weapon
+        }
+    }
+}
+
+const peter = createElf('Peter', 'Stone')
+console.log(peter.attack());
+
+
+const sam = createElf('Sam', 'Sword')
+console.log(sam.attack());

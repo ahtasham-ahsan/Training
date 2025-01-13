0bd96601838b9164766c90ff2348d9cdc5eaa48a
haxhmi1993 <127983564+haxhmi1993@users.noreply.github.com>
2025-01-13 19:46:06 +0500

    updates in es10

--- Folder 15/18 ES7 and ES8.js
+++ Folder 15/18 ES7 and ES8.js
@@ -47,3 +47,62 @@ Object.entries(obj).forEach((e)=>{
 })
 
 
+// ...................................................................................................................
+// ES 10 Updates
+// flat
+const array = [1,2,3,4,5]
+console.log("Nothing happens here", array.flat());
+
+// flats works with nested arrays
+const array2 = [1, [2,3], [4,5], [6,7]];
+console.log("Original Array", array2);
+console.log("Flat works with this nested array", array2.flat());
+
+const array1 = [
+    [1,2, [
+        3,4,[
+            9,10,[
+                "too much nested till here"
+            ]
+        ]
+    ]]
+]
+
+console.log("Original Array", array1);
+console.log("Flat Array", array1.flat());
+console.log("Flat Array with parameters that tells how many layers to go in (By Default it is 1)", array1.flat(4));
+
+
+const entryyyy = [1,,,,,,3]
+console.log(entryyyy);
+console.log(entryyyy.flat());
+
+const userEmail = '        happyeagle@gmail.com';
+const userEmail1 = 'danger_eagle@gmail.com       ';
+
+console.log(userEmail.trimStart());
+console.log(userEmail1.trimEnd());
+
+
+
+// formentries
+const userProfiles = [
+    ['CommanderTom', 22],
+    ['DerekZlander', 23], 
+    ['Hansel', 44]
+]
+
+
+console.log(Object.fromEntries(userProfiles));
+const a =Object.fromEntries(userProfiles)
+console.log(a);
+
+
+// try catch
+try{
+    bob+'hi';
+}
+catch(e){
+    console.log('youu messed up', e);
+    
+}

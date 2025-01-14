85f5ca47da6f8fc0eefcbb8029eaa25866af2bae
haxhmi1993 <127983564+haxhmi1993@users.noreply.github.com>
2025-01-14 15:15:29 +0500

    Imperitive vs Declarative Coding

---
+++ Folder 07/Imperitive_vs_Declarative.js
@@ -0,0 +1,11 @@
+// Imperative vs Declarative Coding
+// Imperative ==> tells the computer what to do and how to do
+
+for (let index = 0; index < 1000; index++) {
+    console.log(index);
+  }
+  
+  // Declarative ==> tells the computer what to do and what should happen...
+  [1,2,3].forEach(item => {
+      console.log(item);
+  })

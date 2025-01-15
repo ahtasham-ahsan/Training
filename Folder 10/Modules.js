b234492d69b45fbe1005923b12332a41cb62275f
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-15 17:18:42 +0500

    Modules in Javascript

---
+++ Folder 10/Modules.js
@@ -0,0 +1,10 @@
+// Modules
+// Modules in JavaScript are reusable pieces of code that can be exported from one file and imported into another. They allow developers to split their code into smaller, more manageable, and reusable files.
+
+import multiply, { add, subtract } from './math.js';    
+
+console.log(add(2, 3));        // 5
+console.log(subtract(7, 2));   // 5
+console.log(multiply(3, 4));   // 12
+
+console.log('Hello  ');

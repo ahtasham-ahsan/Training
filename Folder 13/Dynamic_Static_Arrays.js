29306b3a11fbcd551272044728f8d263132dc797
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-16 17:05:12 +0500

    Dynamic and Static Arrays

---
+++ Folder 13/Dynamic_Static_Arrays.js
@@ -0,0 +1,8 @@
+// Static and Dynamic arrays
+// static - fixed number of elements
+// Dynamic Arrays - Not fixed nummber of items 
+
+// C++ -- Static Arrays 
+// int a[10];
+// string b[2] = ['a','aa']
+

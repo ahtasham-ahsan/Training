a052b62a7b93f883951a8f88a0ec0bd9d88bc23f
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-17 12:37:39 +0500

    Hash Tables and Hash Function

---
+++ index.js
@@ -0,0 +1,11 @@
+// hash Tables
+// Dictionaries - python
+// Maps - Java
+// Objects - Javascript
+// Hashes - Ruby
+// These all are the same 
+
+
+// HASH FUNCTION
+// Generates a fixed length output for the input
+// https://miraclesalad.com/webtools/md5.php

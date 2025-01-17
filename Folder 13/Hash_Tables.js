d3f7fc8211218098dd30131befadd07b610407b8
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-17 12:50:50 +0500

    Hash tables, Hash Functions, Hash Collsions, Hash Tables in JS (Objects)

---
+++ Folder 13/Hash_Tables.js
@@ -0,0 +1,31 @@
+// hash Tables
+// Dictionaries - python
+// Maps - Java
+// Objects - Javascript
+// Hashes - Ruby
+// These all are the same 
+
+
+// HASH FUNCTION
+// Generates a fixed length output for the input
+// https://miraclesalad.com/webtools/md5.php
+
+
+// Hash Tables in JS - Objects
+let user = {
+    name: 'Sarah',
+    age:21,
+    magic:true,
+    scream(){
+        console.log('Ewwwwwww');
+    }
+}
+
+console.log(user.age);
+user.spell = 'Abra ka Dabra';
+console.log(user);
+
+console.log(user.scream());
+
+// Collision
+// Stores two or more datas in the memory at the same location

592c67fb649963102e25e7a174910e3a9a57ee94
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-20 15:42:08 +0500

    Types in Type Script

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -7,4 +7,32 @@ else {
     age -= 5;
 }
 console.log(age);
+var num = 123456780;
+var str = 'TypeScript';
+var is_Published = true;
+// Any - Type
+var level;
+function render(doc) {
+    console.log(doc);
+}
+// Arrays 
+var numbers = [2, 4, 5, 1, 'S'];
+// Tuples
+var user = [1, "Ahtasham"];
+// Enums
+// List of Related Constants
+// One way is
+var small = 1;
+var large = 2;
+var medium = 3;
+// Other way of storing them using enum is 
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Larger"] = 3] = "Larger";
+})(Size || (Size = {}));
+;
+var my_Size = Size.Medium;
+console.log(my_Size);
 //# sourceMappingURL=index.js.map
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -5,3 +5,35 @@ if(age >= 10 ){
     age -= 5;
 }
 console.log(age);
+
+
+let num: number = 123_456_780;
+let str: string = 'TypeScript';
+let is_Published: boolean = true;
+
+// Any - Type
+let level;
+
+function render(doc: any){
+    console.log(doc);
+}
+
+// Arrays 
+let numbers = [2, 4, 5, 1, 'S'];
+
+// Tuples
+let user: [number, string] = [1, "Ahtasham"];
+
+// Enums
+// List of Related Constants
+
+// One way is
+const small = 1;
+const large = 2;
+const medium = 3;
+
+// Other way of storing them using enum is 
+enum Size { Small =1, Medium = 2, Larger = 3};
+
+let my_Size: Size = Size.Medium;
+console.log(my_Size)

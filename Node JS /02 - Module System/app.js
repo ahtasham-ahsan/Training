6859983fc836a36b00f1432ad90eb3f52cf0ea48
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-24 14:47:25 +0500

    Loading a Module

--- Node JS /02 - Module System/app.js
+++ Node JS /02 - Module System/app.js
@@ -1,1 +1,6 @@
-require('./02 - Logger');
+const logger = require('./02 - Logger');
+// logger = 1;
+console.log(logger.log('Logged in Successfully'));
+
+// for exporting a single function instead of a object
+// module.export = log

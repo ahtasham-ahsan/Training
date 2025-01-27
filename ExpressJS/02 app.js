0b6e2581d408d7b6407e3646748014029422e730
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-27 15:40:16 +0500

    handling http requests

--- ExpressJS/02 app.js
+++ ExpressJS/02 app.js
@@ -1,6 +1,11 @@
 const  express = require('express');
 const app = express() // returns a function
-
+const courses = [
+    {id:1, name: "Course_1"},
+    {id:2, name: "Course_2"},
+    {id:3, name: "Course_3"},
+    {id:4, name: "Course_4"},
+];
 // app.get();
 // app.post();
 // app.delete();
@@ -11,13 +16,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([2,3,4]);
     // res.send([2,3,4]);
+    res.send(courses);
 });
 
 // Routes
 app.get('/api/courses/:id', (req, res)=>{
-    res.send(req.params.id);
+    // res.send(req.params.id);
+    const course =courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) //404
+    {
+        res.status(404).send('Course Not Found'); 
+    }
+    else{
+        res.send(course);
+    }
 });
 // Multiple Params
 app.get('/api/:posts/:months', (req, res)=>{

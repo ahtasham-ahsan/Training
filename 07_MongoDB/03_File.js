0ebc7f5da48889305849ec6703702cf29b9c65b4
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-30 18:31:16 +0500

    update method

--- 07_MongoDB/03_File.js
+++ 07_MongoDB/03_File.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const { tail } = require("underscore");
-mongoose.connect("mongodb://localhost:27017/course");
+
+mongoose.connect("mongodb://localhost:28017/course");
 
 const course_Schema = new mongoose.Schema({
   name: String,
@@ -23,18 +23,30 @@ async function createCourse() {
     author: "Dr ABC",
     tags: ["node", "backend"],
     isPublished: true,
-    });
-    const result = await new_course.save();
-    console.log(result);
+  });
+  const result = await new_course.save();
+  console.log(result);
 }
 async function getCourses() {
-    const courses = await course
-        .find({ author: "Dr ABC", isPublished: true })
-        .or([{ price: { $gte:15}}, 
-            { name: /.*by.*/i } 
-        ])
-        .limit(10)
-        .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    console.log(courses);
-    }
+  const courses = await course
+    .find({ author: "Dr ABC", isPublished: true })
+    .or([{ price: { $gte: 15 } }, { name: /.*by.*/i }])
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+async function updateCourse(id) {
+  const courses = await course.findById(id);
+  if (!courses) return;
+  courses.isPublished = true;
+  courses.author = "Dr XYZ";
+  courses.set({
+    name: "Python",
+    price: 20,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+updateCourse("5f3b3b3b3b3b3b3b3b3b3b3b");

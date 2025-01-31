cfdae3c850fe27553f2d2d52bb048fac2c88c92b
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-01-31 15:45:42 +0500

    API for Customers and Saved in the database

--- 08_Data_Validation/04-Customers_API.js
+++ 08_Data_Validation/04-Customers_API.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/Customers', {
+mongoose.connect('mongodb://localhost:28017/Customers', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });

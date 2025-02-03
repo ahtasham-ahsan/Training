973a41ea7497450dc48d5ac906550939b05be77b
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-03 15:39:54 +0500

    packages

--- 09_Modeling_Relationships/01-Modeling-Relationships.js
+++ 09_Modeling_Relationships/01-Modeling-Relationships.js
@@ -19,13 +19,67 @@ let courses_Embedded = {
 
 // Hybrid
 let author_Hybrid = {
+  name: "Mosh",
+  // 50 other properties
+};
+
+let courses_Hybrid = {
+  author_Hybrid: {
+    id: "ref",
     name: "Mosh",
-    // 50 other properties
-    };
-    
-    let courses_Hybrid = {
-        author_Hybrid: {
-            id: "ref",
-            name: "Mosh",
-        },
-    };
+  },
+};
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27018/Test_Server")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const Author = mongoose.model(
+  "Author",
+  new mongoose.Schema({
+    name: String,
+    bio: String,
+    website: String,
+  })
+);
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+  })
+);
+
+async function create_Author(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website,
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function create_Course(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function list_Courses() {
+  const courses = await Course.find().select("name");
+  console.log(courses);
+}
+
+create_Author("Morris", "My bio", "My Website");
+app.listen(port, () => console.log(`Listening on port ${port}...`));

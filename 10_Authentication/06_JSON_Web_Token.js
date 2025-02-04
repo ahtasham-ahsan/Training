1609893edbf36c2faf6a0dad7f8671adbee256db
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-04 15:36:31 +0500

    Components of JSON Web Tokens

---
+++ 10_Authentication/06_JSON_Web_Token.js
@@ -0,0 +1,4 @@
+// Conponents of JWT
+// 1- Header --> Contains the algorithm used and type
+// 2- Payload --> Contains the details of the user
+// 3- Verified Signature --> Contains the digital signature, if change in the token - signature will also change

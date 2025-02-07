7d7af370bda1a28e2d78e9ded5adcdf62bd9e284
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-07 12:35:20 +0500

    logging messages in mongodb - winston-mongodb library - logging levels - error, warn, info, silly, verbose, debug

--- 11_Error_Handling/03_Loggers.js
+++ 11_Error_Handling/03_Loggers.js
@@ -3,13 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const winston = require("winston");
+require("winston-mongodb");
 
 app.use(express.json());
 mongoose.connect("mongodb://localhost:27018/Tester");
 
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
+winston.add(new winston.transports.MongoDB(), {
+  db: "mongodb://localhost:27018/Tester",
+});
 app.use(function (err, req, res, next) {
   winston.log("error", err.message);
+  // Logging Levels
+  // Error, Warn, Info, Verbose, Debug, Silly
   // res.status(500).send("Something went wrong. " + err.message);
 });
 

201a0ecfe80b01b9df83f16775c61ceb7d78f2cc
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-07 11:53:06 +0500

    handling error using middleware

--- 11_Error_Handling/01_Rejected_Promise.js
+++ 11_Error_Handling/01_Rejected_Promise.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 mongoose.connect("mongodb://localhost:27018/Tester");
 
+app.use(function (err, req, res, next) {
+  res.status(500).send("Something went wrong. " + err.message);
+});
+
 const Test_schema = new mongoose.Schema({
   name: String,
   courseTitle: String,
@@ -13,12 +17,13 @@ const Test_schema = new mongoose.Schema({
 
 const Test_Model = mongoose.model("Test", Test_schema);
 
-app.get("/", async (req, res) => {
+app.get("/", async (req, res, next) => {
   try {
     const courses = await Test_Model.find();
     res.send(courses);
   } catch (err) {
-    res.status(500).send(err);
+    // res.status(500).send(err);
+    next(err);
   }
 });
 

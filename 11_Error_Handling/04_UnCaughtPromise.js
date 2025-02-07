f43ff760602dc1274fb46a19e309b1f43e168f3b
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-07 17:51:23 +0500

    Un Handled Exceptions

---
+++ 11_Error_Handling/04_UnCaughtPromise.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+const winston = require("winston");
+require("winston-mongodb");
+
+process.on("unhandledRejection", (ex) => {
+  winston.error(ex.message);
+});
+
+app.use(express.json());
+mongoose.connect("mongodb://localhost:27018/Tester");
+
+winston.add(new winston.transports.File({ filename: "logfile.log" }));
+// winston.add(new winston.transports.MongoDB(), {
+//   db: "mongodb://localhost:27018/Tester",
+// });
+winston.handleExceptions(
+  new winston.transports.File({ filename: "UnhandledExceptions.log" })
+);
+app.use(function (err, req, res, next) {
+  winston.log("error", err.message);
+  // Logging Levels
+  // Error, Warn, Info, Verbose, Debug, Silly
+  // res.status(500).send("Something went wrong. " + err.message);
+});
+const p = Promise.reject(new Error("Failed Miserably"));
+p.then(() => console.log("Done"));
+const Test_schema = new mongoose.Schema({
+  name: String,
+  courseTitle: String,
+});
+
+const Test_Model = mongoose.model("Test", Test_schema);
+
+app.get("/", async (req, res, next) => {
+  const courses = await Test_Model.find();
+  res.send(courses);
+  // res.status(500).send(err);
+});
+
+app.listen(port);

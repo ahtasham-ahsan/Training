aee17f8f9966b054843a745c389875e2171bd73c
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-10 13:07:40 +0500

    Grouping related tests - Describe

--- 12_Unit_Testing/Tests/lib.test.js
+++ 12_Unit_Testing/Tests/lib.test.js
@@ -14,3 +14,23 @@ test("absolute function for Zero", () => {
   const result = lib.absolute(0);
   expect(result).toBe(0); // Matcher Functions
 });
+
+// Grouping Related Tests of a Function.
+// Grouping tests of Absolete Function
+
+describe("Absolute Functions", () => {
+  it("absolute function for Pos Values", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1); // Matcher Functions
+  });
+
+  it("absolute function for Neg Values", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1); // Matcher Functions
+  });
+
+  it("absolute function for Zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0); // Matcher Functions
+  });
+});

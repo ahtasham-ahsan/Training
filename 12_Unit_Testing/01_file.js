e3e1eff312e296d7922a19d86ecf76f1de1af470
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-10 12:47:50 +0500

    types of test, what are tests and frameworks used

---
+++ 12_Unit_Testing/01_file.js
@@ -0,0 +1,18 @@
+// Types of Testing
+// 1- Unit Testing
+// Tests a component of the program with out its external dependencies
+
+// 2 - Integration Tests
+// Test a component of the program with its dependencies like databases files etc.
+
+// 3 - End to End Testing
+// Drives an application through its User Interface (UI).
+// Tools like Selenium is used to end to end testing.
+
+// Test Pyraminx
+// --> Most of the tests should be in Unit Test
+// --> Some tests should be in Integration Test
+// --> Very few tests should be in End to End Testing
+
+// Frameworks
+// Jasmine, Mocha, Jest

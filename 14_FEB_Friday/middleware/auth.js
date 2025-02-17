3bab8f1499d567ebc5393d2f6aa6f32791fe226e
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-02-17 12:31:27 +0500

    middleware

--- 14_FEB_Friday/middleware/auth.js
+++ 14_FEB_Friday/middleware/auth.js
@@ -1,24 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// const secretKey = process.env.SECRET_KEY;
-
-// const authorize = (req, res, next) => {
-//     const token = req.headers['authorization']?.split(' ')[1];
-//     if (!token) {
-//         return res.status(401).send('Unauthorized');
-//     }
-
-//     jwt.verify(token, secretKey, (err, decoded) => {
-//         if (err) {
-//             return res.status(401).send('Unauthorized');
-//         }
-
-//         req.user = decoded;  // Store decoded token in request for use in other routes
-//         next();
-//     });
-// };
-
-// module.exports = authorize;
-
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.SECRET_KEY;
 

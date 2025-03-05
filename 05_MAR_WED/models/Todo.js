f2a0abcb697cbff11c03b975b5a4db1485ba1f56
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-03-05 17:00:40 +0500

    models

---
+++ 05_MAR_WED/models/Todo.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    status: { type: String, enum: ["pending", "completed"], default: "pending" },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Todo", TodoSchema);

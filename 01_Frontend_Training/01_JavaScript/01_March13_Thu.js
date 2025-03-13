4549f1d4100a7bd40d356670c9099506614a5fce
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-03-13 12:08:32 +0500

    Objects

--- 01_Frontend_Training/01_JavaScript/01_March13_Thu.js
+++ 01_Frontend_Training/01_JavaScript/01_March13_Thu.js
@@ -1,3 +1,4 @@
+console.log("\n----------------------- Variables ------------------------\n")
 let experience = false;
 
 function changeExperience() {
@@ -13,7 +14,7 @@ console.log(DontChange);
 
 // Arrow Functions
 
-// Closures
+console.log("\n----------------------- Closures ------------------------\n")
 const first = () => {
     const greet = 'Hello';
     const second = () => {
@@ -26,7 +27,7 @@ const first = () => {
 const newFunc = first();
 newFunc();
 
-// Currying
+console.log("\n----------------------- Curring ------------------------\n")
 // Currying is the process of converting a function that takes multiple arguments into a function that takes them one at a time.
 // Currying is a way of constructing functions that allows partial application of a function’s arguments.
 const multiply = (a, b) => a * b;
@@ -35,30 +36,42 @@ const multiplyBy5 = curriedMultiply(5);
 console.log(multiplyBy5(10));
 console.log(curriedMultiply(2)(3));
 
-// Compose
+console.log("\n----------------------- Compose ------------------------\n")
 const compose = (f, g) => (a) => f(g(a));
 const sum = (num) => num + 1;
 console.log(compose(sum, sum)(5));
 
 
-// Advanced Arrays
+console.log("\n----------------------- Arrays ------------------------\n")
 const arr = [1, 2, 10, 16];
 const double = []
 const newArray = arr.forEach((num) => {
     double.push(num * 2);
 });
-console.log(double);
+console.log("Doubled Array --> ", double);
 
 // map, filter, reduce
 const mapArr = arr.map((num) => num * 2);
-console.log(mapArr);
+console.log("Mapped Array --> ",mapArr);
 
 const filterArr = arr.filter((num) => num % 2 === 0);
-console.log(filterArr);
+console.log("Filtered Array --> ",filterArr);
 
 const reduceArr = arr.reduce((acc, num) => {
     console.log("-------------");
     console.log("--> ", acc, num);
     return acc + num
 }, 0);
-console.log(reduceArr);
+console.log("Reduced Array --> ", reduceArr);
+
+console.log("\n----------------------- Objects ------------------------\n")
+var obj1 = {value: 10};
+var obj2 = obj1;
+var obj3 = {value: 10};
+
+console.log(obj1 === obj2);
+console.log(obj1 === obj3);
+obj2.value = 15;
+console.log(obj1.value);
+console.log(obj2.value);
+console.log(obj3.value);

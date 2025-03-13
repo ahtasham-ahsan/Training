858384183e1b8cef5fc85607cbc23b91a3b65933
Ahtasham <muhammad.ahtasham@ocloudsolutions.net>
2025-03-13 12:41:58 +0500

    object from entries, triming the strings, flating the arrays

--- 01_Frontend_Training/01_JavaScript/01_March13_Thu.js
+++ 01_Frontend_Training/01_JavaScript/01_March13_Thu.js
@@ -1,3 +1,5 @@
+const { object } = require("joi");
+
 console.log("\n----------------------- Variables ------------------------\n")
 let experience = false;
 
@@ -75,3 +77,90 @@ obj2.value = 15;
 console.log(obj1.value);
 console.log(obj2.value);
 console.log(obj3.value);
+console.log("\n----------------------- Instantiation ------------------------\n")
+class Player{
+    constructor(name, type){
+        console.log(this);
+        this.name = name;
+        this.type = type;
+    }
+    introduce(){
+        console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+    }
+}
+
+class Wizard extends Player{
+    constructor(name, type){
+        super(name, type);
+    }
+    play(){
+        console.log(`WEEEEEEE I'm a ${this.type}`);
+    }
+}
+
+const wizard1 = new Wizard('Shelly', 'Healer');
+const wizard2 = new Wizard('Shawn', 'Dark Magic');
+
+wizard1.introduce();
+wizard1.play();
+wizard2.introduce();
+wizard2.play();
+
+console.log("\n----------------------- ES 07 ------------------------\n")
+const pets = ['cat', 'dog', 'bat'];
+console.log(pets.includes('dog'));
+console.log(pets.includes('lizard'));
+
+const square = (x) => x**2;
+const cube = (y) => y**3;   
+console.log(square(2));
+console.log(cube(3));
+
+console.log("\n----------------------- ES 08 ------------------------\n")
+console.log('Turtle'.padStart(10));
+console.log('Turtle'.padEnd(10));
+
+const fun = (
+    a, 
+    b, 
+    c, 
+    d,
+) => {
+    console.log(a);
+}
+fun(1,2,3,4,);
+
+
+let obj  = {
+    username0: "Santa",
+    username1: "Rudolf",
+    username2: "Mr Grinch"
+}
+
+Object.keys(obj).forEach((key, index) => {
+    console.log(index+1, "--> ",key, obj[key]);
+})
+
+Object.values(obj).forEach(value => {
+    console.log(" -- ", value);
+});
+
+Object.entries(obj).forEach(val =>{
+    console.log(" -- ",val);
+});
+
+console.log("\n----------------------- ES 08 ------------------------\n")
+const array = [1,2,3,4, [3,4,5]];
+console.log(array.flat());
+
+const entery = ['bob', 'sally', , , , , 'cindy'];
+console.log(entery.flat());
+
+const userEmail1 = "           eddytheeagle@gmail.com";
+const userEmail2 = "johnDangerous@gmaill.com                                      " ;
+console.log("--------------------\n",userEmail1.trimStart());
+console.log(userEmail2.trimEnd());
+
+const userProfiles = [['commanderTom', 23], ['derekZlander', 40], ['hansel', 18]];
+const obj4 = Object.fromEntries(userProfiles);
+console.log("--------------------\nObject From Entries \n\n",obj4);
